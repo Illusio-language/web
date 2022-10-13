e78e499cdc8ce45ae05a30442d03e40dd04829d6
dinxsh <talwadkerdinesh@gmail.com>
2022-10-13 08:58:49 +0530

    added doc page and updated index page

--- pages/components/download.js
+++ pages/components/download.js
@@ -2,7 +2,7 @@ import Image from 'next/image';;
 import Link from 'next/link';
 const Download = () => {
   return (
-<p>Download</p>
+  <p></p>
   );
 };
 
--- pages/docs.js
+++ pages/docs.js
@@ -1,14 +1,10 @@
-import Head from "next/head";
 import Nav from "./components/nav";
 import Footer from "./components/Footer";
-import Header from "./components/header";
-import Download from "./components/Download";
 
 export default function Home() {
   return (
     <>
       <Nav />
-      <Header />
       <Footer />
     </>
   );
