ea1c633ca33e215ac4d5259c5e43f80b989fcc22
dinxsh <talwadkerdinesh@gmail.com>
2022-10-13 09:26:22 +0530

    added docs page and added accordion to docs page | added babel-helper dependency and react-scripts

---
+++ pages/components/accordion.js
@@ -0,0 +1,21 @@
+import ScriptTag from 'react-script-tag';
+import Image from 'next/image';;
+import Link from 'next/link';
+const Download = () => {
+  return (
+    <div>
+        <ScriptTag isHydrating={true} type="text/javascript" src="../../scripts/accordion.js" />
+        <button class="accordion">Section 1</button>
+        <div class="panel">
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque eveniet
+            deleniti id, tenetur ullam ipsam vitae sunt, laudantium temporibus
+            consequatur architecto a magni sit tempora ea error veritatis aperiam.
+            Illum.
+        </p>
+        </div>
+    </div>
+  );
+};
+
+export default Download
--- pages/docs.js
+++ pages/docs.js
@@ -1,11 +1,14 @@
 import Nav from "./components/nav";
 import Footer from "./components/Footer";
+import accordion from "./components/accordion";
+import Download from "./components/download";
 
 export default function Home() {
   return (
     <>
       <Nav />
       <Footer />
+      <Download />
     </>
   );
 }
