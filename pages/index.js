9cfcb28f11e3bd32da946ebc992cbf73df00ed36
Sri Vamsi Rajesh <83352740+SrivamsiRajesh@users.noreply.github.com>
2022-10-27 19:50:30 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,12 @@ import Head from "next/head";
 import Nav from "../components/nav";
 import Footer from "../components/Footer";
 import Header from "../components/header";
-
+import Head from 'next/head'
 export default function Home() {
   return (
+     <Head>
+      <title>Illusio</title>
+    </Head>
     <>
       <Nav />
       <Header />
